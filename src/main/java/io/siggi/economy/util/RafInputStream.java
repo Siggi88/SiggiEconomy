2786af8eb16febcdd4e34c4cf775c8f293deae44
Siggi <7286178+Siggi88@users.noreply.github.com>
2021-09-03 16:23:47 +0000

    Add read limit and ability to read lines to RafInputStream

--- src/main/java/io/siggi/economy/util/RafInputStream.java
+++ src/main/java/io/siggi/economy/util/RafInputStream.java
@@ -1,12 +1,15 @@
 package io.siggi.economy.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
 
 public class RafInputStream extends InputStream {
 
 	private final RandomAccessFile raf;
+	private int limit;
 
 	public RafInputStream(RandomAccessFile raf) {
 		this.raf = raf;
@@ -14,16 +17,40 @@ public class RafInputStream extends InputStream {
 
 	@Override
 	public int read() throws IOException {
+		if (limit >= 0) {
+			if (limit == 0) {
+				return -1;
+			}
+			limit -= 1;
+		}
 		return raf.read();
 	}
 
 	@Override
 	public int read(byte[] b) throws IOException {
-		return raf.read(b);
+		if (limit < 0) {
+			return raf.read(b);
+		}
+		if (limit >= 0) {
+			if (limit == 0) {
+				return -1;
+			}
+		}
+		int maxRead = Math.min(limit, b.length);
+		int amountRead = raf.read(b, 0, limit);
+		if (amountRead >= 0)
+			limit -= amountRead;
+		return amountRead;
 	}
 
 	@Override
 	public int read(byte[] b, int offset, int length) throws IOException {
+		if (limit >= 0) {
+			if (limit == 0) {
+				return -1;
+			}
+			limit -= 1;
+		}
 		return raf.read(b, offset, length);
 	}
 
@@ -34,6 +61,33 @@ public class RafInputStream extends InputStream {
 			return Integer.MAX_VALUE;
 		}
 		return (int) a;
+	}
+
+	public void setLimit(int limit) {
+		if (limit < 0) limit = -1;
+		this.limit = limit;
+	}
+
+	public void skipLine() throws IOException {
+		while (true) {
+			int i = read();
+			if (i == -1 || i == 0x0A) break;
+		}
+	}
+
+	private ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+	public String readLine() throws IOException {
+		baos.reset();
+		while (true) {
+			int i = read();
+			if (i == -1 || i == 0x0A) {
+				if (i == -1 && baos.size() == 0) return null;
+				break;
+			}
+			baos.write(i);
+		}
+		return new String(baos.toByteArray(), StandardCharsets.UTF_8);
 	}
 
 }
